eb1240aff4a2def66290e643aa38ec96a06cbb0c
KevinP <kevinpcho098@gmail.com>
2026-04-23 14:57:39 -0600

    CRUD completo contactos

--- server.js
+++ server.js
@@ -34,6 +34,43 @@ app.post("/api/contacto", async (req, res) => {
   }
 });
 
+app.get("/api/contacto", async (req, res) => {
+    try {
+      const contactos = await Contacto.find();
+      res.json(contactos);
+    } catch (error) {
+      res.status(500).json({ error: "Error al obtener contactos" });
+    }
+  });
+
+  app.put("/api/contacto/:id", async (req, res) => {
+    try {
+      const actualizado = await Contacto.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+      );
+  
+      res.json({
+        mensaje: "Contacto actualizado",
+        data: actualizado
+      });
+    } catch (error) {
+      res.status(500).json({ error: "Error al actualizar" });
+    }
+  });
+
+  app.delete("/api/contacto/:id", async (req, res) => {
+    try {
+      await Contacto.findByIdAndDelete(req.params.id);
+  
+      res.json({
+        mensaje: "Contacto eliminado"
+      });
+    } catch (error) {
+      res.status(500).json({ error: "Error al eliminar" });
+    }
+  });
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("MongoDB conectado");
